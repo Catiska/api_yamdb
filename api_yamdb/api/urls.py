2b1cc45795c71c58aa1db49f02a28e6abf862726
Catiska <katrinka5@yandex.ru>
2023-02-28 16:55:44 +0300

    Add ReviewViewSet and CommentViewSet to api/views

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+
+from rest_framework import routers
+
+from views import ReviewViewSet, CommentViewSet
+
+app_name = 'api'
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls))
+]
---
+++ api_yamdb/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+from serializers import ReviewSerializer, CommentSerializer
+from reviews.models import Review, Comment
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    # permission_classes = (,)
+
+    def get_queryset(self):
+        return get_object_or_404(
+            Review, id=self.kwargs.get('title_id')
+        ).reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Review, id=self.kwargs.get('title_id'))
+        serializer.save(
+            author=self.request.user,
+            title=title
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    # permission_classes = (,)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
