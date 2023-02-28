b7584ce45380623850aef40bfd17e8c49c7c9006
Catiska <katrinka5@yandex.ru>
2023-02-28 16:44:02 +0300

    Add Review and Comment models

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,56 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Review(models.Model):
+    """Модель отзыва на произведение."""
+    text = models.CharField('Текст отзыва', max_length=500)
+    score = models.IntegerField(
+        'Оценка',
+        validators=(
+            MinValueValidator(1, 'Минимальная оценка - 1'),
+            MaxValueValidator(10, 'Максимальная оценка - 10')
+        )
+    )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='reviews')
+    title = models.ForeignKey(Title,
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    pub_date = models.DateField('Дата публикации',
+                                auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('author', 'title'),
+                name='unique review'
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:30]
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву."""
+    text = models.CharField('Текст комментария', max_length=200)
+    pub_date = models.DateField('Дата публикации', auto_now_add=True)
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[:15]
