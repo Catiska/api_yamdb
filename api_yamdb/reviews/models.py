4851705d8ed4006b4b51447c970f402c3acec6a8
Catiska <katrinka5@yandex.ru>
2023-03-01 14:08:41 +0300

    Move Review models to bot

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,60 +4,6 @@ from django.db.models.constraints import UniqueConstraint
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Review(models.Model):
-    """Модель отзыва на произведение."""
-    text = models.CharField('Текст отзыва', max_length=500)
-    score = models.IntegerField(
-        'Оценка',
-        validators=(
-            MinValueValidator(1, 'Минимальная оценка - 1'),
-            MaxValueValidator(10, 'Максимальная оценка - 10')
-        )
-    )
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='reviews')
-    title = models.ForeignKey(Title,
-                              on_delete=models.CASCADE,
-                              related_name='reviews')
-    pub_date = models.DateTimeField('Дата публикации',
-                                auto_now_add=True)
-
-    class Meta:
-        ordering = ('-pub_date',)
-        verbose_name = 'Отзыв'
-        verbose_name_plural = 'Отзывы'
-        constraints = [
-            models.UniqueConstraint(
-                fields=('author', 'title'),
-                name='unique review'
-            )
-        ]
-
-    def __str__(self):
-        return self.text[:30]
-
-
-class Comment(models.Model):
-    """Модель комментария к отзыву."""
-    text = models.CharField('Текст комментария', max_length=200)
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    review = models.ForeignKey(Review,
-                               on_delete=models.CASCADE,
-                               related_name='comments')
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='comments')
-
-    class Meta:
-        ordering = ('-pub_date',)
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return self.text[:15]
-
-
 class Category(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название категории')
     slug = models.SlugField(unique=True, max_length=50, verbose_name='Слаг категории')
@@ -146,3 +92,59 @@ class User(AbstractUser):
     @property
     def is_moderator(self):
         return self.role == "moderator"
+
+
+class Review(models.Model):
+    """Модель отзыва на произведение."""
+    text = models.CharField('Текст отзыва', max_length=500)
+    score = models.IntegerField(
+        'Оценка',
+        validators=(
+            MinValueValidator(1, 'Минимальная оценка - 1'),
+            MaxValueValidator(10, 'Максимальная оценка - 10')
+        )
+    )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='reviews')
+    title = models.ForeignKey(Title,
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    pub_date = models.DateTimeField('Дата публикации',
+                                auto_now_add=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('author', 'title'),
+                name='unique review'
+            )
+        ]
+
+    def __str__(self):
+        return self.text[:30]
+
+
+class Comment(models.Model):
+    """Модель комментария к отзыву."""
+    text = models.CharField('Текст комментария', max_length=200)
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text[:15]
+
+
