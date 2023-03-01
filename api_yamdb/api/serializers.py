9b51c60e4b6636860411f41bdc7f09b2def356de
Catiska <katrinka5@yandex.ru>
2023-03-01 09:56:39 +0300

    Change __all__ to list of fields in CommentSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -27,7 +27,6 @@ class ReviewSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """Проверка повторного отзыва к текущему произведению."""
         request = self.context['request']
-        # author = request.user
         title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
         if (
@@ -45,6 +44,6 @@ class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор можели комментариев к отзыву."""
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('id', 'author', 'text', 'pub_date', 'review')
         read_only_fields = ('author', 'review')
 
