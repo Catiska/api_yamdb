da5740eeca0c6e8d60c2b7caaa38957a4dceaa5b
Catiska <katrinka5@yandex.ru>
2023-03-01 12:17:20 +0300

    Delete double imports in api/serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,4 @@
-import re
-from django.core.exceptions import ValidationError
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
 from django.core.files.base import ContentFile
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
