eaafb21cfac3f538c094e7849b5aa1e029cdc94d
Catiska <katrinka5@yandex.ru>
2023-03-03 14:20:41 +0300

    Fix misstype in docstring

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -122,7 +122,7 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    """Сериализатор можели комментариев к отзыву."""
+    """Сериализатор модели комментариев к отзыву."""
     author = serializers.SlugRelatedField(slug_field='username',
                                           read_only=True)
 
